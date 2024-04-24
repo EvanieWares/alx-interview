c7277333cde132f4dbd425ee905ffc348c29c90f
EvanieWares <evaniewares@gmail.com>
2024-04-25 01:25:11 +0200

    minimum operations - cleanup

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -7,23 +7,6 @@ operations needed to result in exactly n H characters in the file.
 """
 
 
-def isPrimeNumber(num):
-    """
-    Checks if a number is a prime number.
-
-    Parameters:
-    - num (int): The number to check
-
-    Returns:
-    - True if the number is a prime number
-    - False if the number is not a prime number
-    """
-    for i in range(2, round(num / 2)):
-        if num % i == 0:
-            return False
-    return True
-
-
 def minOperations(n):
     """
     Calculates the fewest number of operations needed to result in exactly
@@ -36,6 +19,6 @@ def minOperations(n):
     - The fewest number of operations
     """
     for i in range(2, n + 1):
-        if isPrimeNumber(i) and n % i == 0:
+        if n % i == 0:
             return i + minOperations(int(n / i))
     return 0
